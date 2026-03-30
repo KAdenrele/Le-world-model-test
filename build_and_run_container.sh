3b83323bd77215409d86e81a73df36f85dedd49a
KAdenrele <akomolafe@zaizi.com>
2026-03-30 16:25:19 +0100

    updated run script

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -1,4 +1,4 @@
-docker build -t lewm:latest -f Dockerfile . && \
+docker build -t lewm:latest -f dockerfile . && \
 docker run  --name lewm_container --gpus all \
 -v /mnt/data/lewm/files:/app/files \
 lewm:latest
