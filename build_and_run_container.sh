0ae1567dc4a3b24680405f52f281dde5ec5c38d2
KAdenrele <akomolafe@zaizi.com>
2026-03-31 23:08:53 +0100

    modified train .py optimizers bracket

--- build_and_run_container.sh
+++ build_and_run_container.sh
@@ -1,5 +1,6 @@
-docker build -t lewm:latest -f dockerfile . && docker run  --name lewm_container \
-                                                           --gpus all \
-                                                           -v /mnt/data/lewm/files:/app/files \
-                                                           --env-file .env \
-                                                           lewm:latest
+docker build -t lewm:latest -f dockerfile . && docker run  \
+                                                    --name lewm_container \
+                                                    --gpus all \
+                                                    -v /mnt/data/lewm/files:/app/files \
+                                                    --env-file .env \
+                                                    lewm:latest
